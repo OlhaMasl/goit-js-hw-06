d5960e7f5b956eca4309e1cf082de61e7f335c22
OlhaMasl <maslakok@gmail.com>
2024-04-07 17:26:10 +0300

    Homework-js-6-version1

---
+++ js/task-2.js
@@ -0,0 +1,25 @@
+class Storage {
+    #items;
+    constructor(productList) {
+        this.#items = productList;
+    }
+
+    getItems() {
+        return this.#items;
+    }
+
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+
+    removeItem(itemToRemove) {
+        this.#items = this.#items.filter(oneItem => oneItem !== itemToRemove);
+    }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
